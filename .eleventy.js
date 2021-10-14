909d2c8fddb9e95d8c5ed707509da46742cf33be
Joseph Petrillo <jpetrillo119@gmail.com>
2021-10-14 11:56:03 -0400

    fix dates

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/assets");
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).plus({ days: 1 }).toLocaleString(DateTime.DATE_MED);
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
 
     return {
