44657351a0051f26f0417cfb057c19a3facca0ed
Joseph Petrillo <jpetrillo119@gmail.com>
2022-01-02 13:41:13 -0500

    update bugged styles

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,6 @@
 const burger = document.querySelector('.nav-btn');
 const nav_bg = document.querySelector('.nav-wrapper');
-const body = document.querySelector('body');
+// const body = document.querySelector('body');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('is-active');
